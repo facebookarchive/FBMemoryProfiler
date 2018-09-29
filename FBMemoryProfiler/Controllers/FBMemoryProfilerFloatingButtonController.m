d61ac13f4994b7e8d3bb9ee2c4559bb2c3351062
yangjianxiang <yangjianxiang@cd.tuan800.com>
2018-09-29 14:34:03 +0800

    show fps support

--- FBMemoryProfiler/Controllers/FBMemoryProfilerFloatingButtonController.m
+++ FBMemoryProfiler/Controllers/FBMemoryProfilerFloatingButtonController.m
@@ -18,6 +18,11 @@
 
   NSTimer *_timer;
   NSByteCountFormatter *_formatter;
+  NSString *_memoryText;
+    
+  CADisplayLink *_link;
+  NSUInteger _count;
+  NSTimeInterval _lastTime;
 }
 
 - (instancetype)init
@@ -30,26 +35,27 @@
 
 - (void)loadView
 {
-  _floatingButton = [UIButton buttonWithType:UIButtonTypeCustom];
-
-  self.view = _floatingButton;
+    _floatingButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    self.view = _floatingButton;
 }
 
 - (void)viewDidLoad
 {
   [super viewDidLoad];
-
-  _floatingButton.backgroundColor = [UIColor lightGrayColor];
+    
+  _floatingButton.titleLabel.numberOfLines = 0;
+  _floatingButton.backgroundColor = [UIColor colorWithWhite:0.000 alpha:0.700];
   _floatingButton.titleLabel.font = [UIFont systemFontOfSize:11];
   _floatingButton.titleLabel.textColor = [UIColor whiteColor];
-  _floatingButton.layer.borderWidth = 1.0;
-  _floatingButton.layer.borderColor = [UIColor grayColor].CGColor;
-  _floatingButton.alpha = 0.8;
-  [_floatingButton setTitle:@"IMP" forState:UIControlStateNormal];
-
+  _floatingButton.clipsToBounds = YES;
+  _floatingButton.layer.cornerRadius = 5;
+  [_floatingButton setTitle:@"IMP/FPS" forState:UIControlStateNormal];
+    
   [_floatingButton addTarget:self action:@selector(buttonTapped) forControlEvents:UIControlEventTouchUpInside];
 
   [self _update];
+  [self _setupFPS:60];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -61,6 +67,9 @@
                                           selector:@selector(_update)
                                           userInfo:nil
                                            repeats:YES];
+    
+  _link = [CADisplayLink displayLinkWithTarget:self selector:@selector(tick:)];
+  [_link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
@@ -69,11 +78,60 @@
 
   [_timer invalidate];
   _timer = nil;
+    
+  [_link invalidate];
+  _link = nil;
 }
 
-- (void)_update
-{
-  [_floatingButton setTitle:[_formatter stringFromByteCount:FBMemoryProfilerResidentMemoryInBytes()] forState:UIControlStateNormal];
+- (void)_update {
+    _memoryText = [_formatter stringFromByteCount:FBMemoryProfilerResidentMemoryInBytes()];
+}
+
+- (void)tick:(CADisplayLink *)link {
+    if (_lastTime == 0) {
+        _lastTime = link.timestamp;
+        return;
+    }
+    
+    _count++;
+    NSTimeInterval delta = link.timestamp - _lastTime;
+    if (delta < 1)  {
+        return;
+    }
+    
+    _lastTime = link.timestamp;
+    CGFloat fps = _count / delta;
+    _count = 0;
+    
+    [self _setupFPS:fps];
+}
+
+- (void)_setupFPS:(CGFloat)fps {
+    if (_memoryText.length == 0) {
+        return;
+    }
+    
+    CGFloat progress = fps / 60.0;
+    UIColor *color = [UIColor colorWithHue:0.27 * (progress - 0.2) saturation:1 brightness:0.9 alpha:1];
+    
+    NSString *impStr = _memoryText;
+    NSString *fpsStr = [NSString stringWithFormat:@"%d FPS",(int)round(fps)];
+    
+    NSString *str = [NSString stringWithFormat:@"%@\n%@", impStr, fpsStr];
+    NSMutableAttributedString *ma = [[NSMutableAttributedString alloc] initWithString:str];
+    [ma addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:11] range:NSMakeRange(0, impStr.length)];
+    [ma addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor] range:NSMakeRange(0, impStr.length)];
+    [ma addAttribute:NSForegroundColorAttributeName value:color range:NSMakeRange(impStr.length +  1, fpsStr.length - 3)];
+    [ma addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor] range:NSMakeRange(str.length - 3, 3)];
+    [ma addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:11] range:NSMakeRange(impStr.length +  1, fpsStr.length)];
+    [ma addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:4] range:NSMakeRange(str.length - 4, 1)];
+    
+    NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+    style.lineSpacing = 2;
+    style.alignment = NSTextAlignmentCenter;
+    [ma addAttribute:NSParagraphStyleAttributeName value:style range:NSMakeRange(0, ma.length)];
+    
+    [_floatingButton setAttributedTitle:ma forState:UIControlStateNormal];
 }
 
 - (void)buttonTapped
